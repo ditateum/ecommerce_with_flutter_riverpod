fe4419d9dad78d54f2bceda7d74506b359b6292a
ditateum <ditateum@gmail.com>
2023-01-05 22:53:51 +0700

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+// ignore:depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
+  // * Register error handlers. For more info, see:
+  // * https://docs.flutter.dev/testing/errors
+  registerErrorHandlers();
+  // * Entry point of the app
+  runApp(const MyApp());
+}
+
+void registerErrorHandlers() {
+  // * Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+  // * Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    debugPrint(error.toString());
+    return true;
+  };
+  // * Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('An error occurred'.hardcoded),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
+}
