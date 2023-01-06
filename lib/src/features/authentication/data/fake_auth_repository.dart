0952bca6a4ac1219c9b598a1a30c1f5089372026
ditateum <ditateum@gmail.com>
2023-01-06 10:14:16 +0700

    create a fake authentication repository

---
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FakeAuthRepository {
+  Stream<AppUser?> authStateChanges() =>
+      Stream.value(null); // TODO: Implement Update
+
+  AppUser? get currentUser => null; // TODO: Implement Update
+
+  Future<void> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    //TODO: Implement
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    //TODO: Implement
+  }
+
+  Future<void> signOut() async {
+    //TODO: Implement
+  }
+}
+
+final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+  return FakeAuthRepository();
+});
+
+final authStateChangesRepository = StreamProvider((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return authRepository.authStateChanges();
+});
